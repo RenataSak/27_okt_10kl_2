07f663d4d2431a4c7fdd0a36aa4b677b7eaeb9f0
RenataSak <renasaku@madona.edu.lv>
2025-10-27 11:54:40 +0200

    Update uzd2.py
    Izlabots

--- uzd2.py
+++ uzd2.py
@@ -2,5 +2,5 @@ battery= 20 # sākumā ir 20%
 # risinājumam izmantojam While konstrukciju
     while battery<100:
     print("Lādējam ...baterija ir", battery,"%")
-    battery=20 # katru reizi pieaug pa 20% ... battery=battery+20
-prit ("Telefons ir pilnībā uzlādēts!")
+    battery+=20 # katru reizi pieaug pa 20% ... battery=battery+20
+print ("Telefons ir pilnībā uzlādēts!")
